851c1524ddaed984b6be18eb451896cca499e9ef
Phillip William <phillipwilliam@gmail.com>
2019-10-08 12:23:48 +1100

    webpack css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,38 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  watch: true,
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            include: [
-              process.cwd() + '/src/'
-            ],
-            loader: 'babel-loader'
-        }
+    entry: './src/index.js',
+    watch: true,
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: [
+                    process.cwd() + '/src/'
+                ],
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: process.cwd() + '/src/index.html',
+            filename: 'index.html'
+        })
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-        template: process.cwd() + '/src/index.html',
-        filename: 'index.html'
-    })
-  ]
 };
